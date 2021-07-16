aea969816c6cecb7d18ba0f23129be9603ce6db6
supriyavallepu <87513486+supriyavallepu@users.noreply.github.com>
2021-07-16 15:19:45 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,181 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import Firebase from "firebase";
+// const config = {
+//   Projecname:myapplication
+// ProjectID :
+// myapplication-9e0c4
+// Projectnumber :
+// 1026547069934
+
+// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    Firebase.initializeApp(config);
+    this.state = {
+      developers: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.writeUserData();
+    }
+  }
+
+  writeUserData = () => {
+    Firebase.database()
+      .ref("/")
+      .set(this.state);
+    console.log("DATA SAVED");
+  };
+
+  getUserData = () => {
+    let ref = Firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let name = this.ref.name.value;
+    let age= this.ref.age.value;
+    let adress= this.ref.adress.value;
+
+    if (name && age && adress) {
+      const { developers } = this.state;
+      const devIndex = developers.findIndex(data => {
+        return data.adress === adress;
+      });
+      developers[devIndex].name = name;
+      developers[devIndex].age= age;
+      developers[devIndex].adress = adress;
+      this.setState({ developers });
+    } else if (name && adress) {
+      const uid = new Date().getTime().toString();
+      const { developers } = this.state;
+      developers.push({ name, age, adress });
+      this.setState({ developers });
+    }
+
+    this.ref.name.value = "";
+    this.ref.age.value = "";
+    this.ref.adress.value = "";
+  };
+
+  removeData = developer => {
+    const { developers } = this.state;
+    const newState = developers.filter(data => {
+      return data.adress!== developer.adress;
+    });
+    this.setState({ developers: newState });
+  };
+
+  updateData = developer => {
+    this.ref.name.value = developer.name;
+    this.ref.age.value = developer.age;
+    this.ref.adress.value = developer.adress;
+  };
+
+  render() {
+    const { developers } = this.state;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              <h1>Firebase Development Team</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xl-12">
+              {developers.map(developer => (
+                <div
+                  key={developer.adress}
+                  className="card float-left"
+                  style={{ width: "18rem", marginRight: "1rem" }}
+                >
+                  <div className="card-body">
+                    <h5 className="card-title">{developer.name}</h5>
+                    <p className="card-text">{developer.age}</p>
+                    <button
+                      onClick={() => this.removeData(developer)}
+                      className="btn btn-link"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => this.updateData(developer)}
+                      className="btn btn-link"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xl-12">
+              <h1>Add new team member here</h1>
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-row">
+                  <input type="hidden" ref="uid" />
+                  <div className="form-group col-md-6">
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      ref="name"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label>age</label>
+                    <input
+                      type="text"
+                      ref="role"
+                      className="form-control"
+                      placeholder="Role"
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label>adress</label>
+                    <input
+                      type="text"
+                      ref="role"
+                      className="form-control"
+                      placeholder="Role"
+                    />
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xl-12">
+              <h3>
+                Tutorial{" "}
+                <a href="https://www.educative.io/edpresso/firebase-as-simple-database-to-react-app">
+                  here
+                </a>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
